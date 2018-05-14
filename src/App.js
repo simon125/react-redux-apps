04cb68b30713bb5e8bcc2bea687d65c8c15dd84d
Szymon Oleszek <szymqwerty@gmail.com>
2018-05-14 11:13:11 +0200

    initial commit created store.js and first reducer

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        REDUX
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/state/counter.js
@@ -0,0 +1,7 @@
+const initialState = 0
+
+export default (state = initialState, action) => {  //jeżeli nie ma nic w state to uzyj stanu defaultowego initailState
+
+    return state
+
+} 
